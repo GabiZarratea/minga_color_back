eb79706ae2e8691ae242818bb5766a83bb433efd
Gabriela Zarratea <71782924+GabiZarratea@users.noreply.github.com>
2023-07-06 20:14:04 -0300

    Cambio de mensajes personalizados (#19)

--- schemas/auth/register.js
+++ schemas/auth/register.js
@@ -1,34 +1,33 @@
-import joi from 'joi'
+import joi from 'joi';
 
 const userRegister = joi.object({
-    email: joi.string()
-              .required()
-              .email({minDomainSegments: 2})
-              .messages({
-                'any.required':'NAME_REQUIRED',
-                'string.empty':'NAME_REQUIRED',
-                'string.email':'INVALID_MAIL'
-              }),
-   photo: joi.string()
-              .required()
-              .uri()
-              .messages({
-                'any.required':'NAME_REQUIRED',
-                'string.empty':'NAME_REQUIRED',
-                'string.uri':'INVALID_URL'
-              }),
-    password: joi.string()
-                 .required()
-                 .min(8)
-                 .max(35)
+  email: joi.string()
+    .required()
+    .email({ minDomainSegments: 2 })
+    .messages({
+      'any.required': 'Email is required',
+      'string.empty': 'Email is required',
+      'string.email': 'Invalid email',
+    }),
+  photo: joi.string()
+    .required()
+    .uri()
+    .messages({
+      'any.required': 'Photo is required',
+      'string.empty': 'Photo is required',
+      'string.uri': 'Invalid photo URL',
+    }),
+  password: joi.string()
+    .required()
+    .min(8)
+    .max(35)
+    .alphanum()
+    .messages({
+      'any.required': 'Password is required',
+      'string.empty': 'Password is required',
+      'string.min': 'Password is too short',
+      'string.max': 'Password is too long',
+    }),
+});
 
-                 .alphanum()
-                 .messages({
-                    'any.required':'NAME_REQUIRED',
-                    'string.empty':'NAME_REQUIRED',
-                    'string.min':'NAME_TOO_SHORT',
-                    'string.max':'NAME_TOO_LARGE'
-                 })
-})
-
-export default userRegister
+export default userRegister;
