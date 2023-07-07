91182c743795ed86d2f1c050c3fff9a6e92573bb
Donato Zollo <davinazarra@gmail.com>
2023-07-07 11:49:26 -0300

    Completo el endpoint de la tarea M-07

--- routes/chapters.js
+++ routes/chapters.js
@@ -1,11 +1,12 @@
 import Router from 'express';
 import read from '../controllers/chapters/read.js'
 import verifyAuthor from '../middlewares/isPropertyOf.js';
+import passport from '../middlewares/passport.js';
 
 let chapter_router = Router();
 
 // chapter_router.post() //crea un autor
-chapter_router.get('/', read)
+chapter_router.get('/', passport.authenticate('jwt', { session:false }), read)
 chapter_router.post('/chapters', verifyAuthor)
 
 // chapter_router.purge() //actualizar un autor
--- routes/mangas.js
+++ routes/mangas.js
@@ -1,12 +1,13 @@
 import Router from 'express';
-import read from '../controllers/mangas/read.js'
+import read from '../controllers/mangas/read_one.js';
+import passport from '../middlewares/passport.js';
 
-let manga_router = Router();
+const manga_router = Router();
 
-// manga_router.post() //crea un autor
-manga_router.get('/', read) //leer uno o todos
-// manga_router.purge() //actualizar un autor
-// manga_router.delete() //elimina un autor
+// manga_router.post() 
+manga_router.get('/:id', passport.authenticate('jwt', { session:false }), read) 
+// manga_router.purge() //actualizar un manga
+// manga_router.delete() //elimina un manga
 
 
 export default manga_router;
