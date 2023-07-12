8c2a06a403626be5262ce55f58e52a19553c4c13
Gabriela Zarratea <71782924+GabiZarratea@users.noreply.github.com>
2023-07-12 10:53:43 -0300

    M-07 solo falta implementar passport (#22)

--- models/Manga.js
+++ models/Manga.js
@@ -2,12 +2,12 @@ import {Schema, model, Types} from "mongoose"
 
 let collection = "mangas"
 let schema = new Schema({ //defino el primer objeto con las propiedades necesarias para el modelo
-    author_id: {type: String, required: true},
+    author_id: {type: Types.ObjectId, ref: 'authors', required: true},
     company_id: {type: String},
     title: {type: String},
     cover_photo: {type: String, required: true},
     description:{type: String},
-    category_id:{type: String, required:true}
+    category_id:{type: Types.ObjectId, ref: 'categories', required:true}
 } , {
     timestamps: true
 })
